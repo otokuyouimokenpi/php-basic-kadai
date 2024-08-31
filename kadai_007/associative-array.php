0cdcc9ae812e6c26520e128395e37814cb0118ea
otokuyouimokenpi <mksr74dc@gmail.com>
2024-08-31 21:46:02 +0900

    課題７追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>７章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $vagetable_date =['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($vagetable_date);
+    ?>
+  </p>
+</body>
+
+</html>
